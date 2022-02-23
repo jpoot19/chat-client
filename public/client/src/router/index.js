32001e3d45967e04004c9e1d0d3412a16a05f205
Jonathan Poot <jpoot@devtzal.com>
2022-02-23 16:33:56 -0500

    Remove # icon in route

--- public/client/src/router/index.js
+++ public/client/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
 const routes = [
@@ -18,9 +18,9 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
-  mode:"hash",
+  mode:"history",
   base:"/wordpress",
 })
 
